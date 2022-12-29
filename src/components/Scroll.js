5c19f554bd6f77b2a7395741d47a1f48bd073d1d
eloquence98 <ibneenayat1998@gmail.com>
2022-12-29 05:55:15 +0500

    small change inside Scroll.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,16 +3,7 @@ import React, { Component } from "react";
 export class Scroll extends Component {
   render() {
     const { children } = this.props;
-    return (
-      <div
-      className="scroll-bar"
-        style={{
-          
-        }}
-      >
-        {children}
-      </div>
-    );
+    return <div className="scroll-bar">{children}</div>;
   }
 }
 
